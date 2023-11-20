9c7b288d476dbe31b70777d0a9083cd45c21e7f9
calleighwinberg <calleighwinberg@gmail.com>
2023-11-19 17:04:23 -0800

    updates to controller

--- HW8_StudentManagementSystem/src/Controller.java
+++ HW8_StudentManagementSystem/src/Controller.java
@@ -8,6 +8,7 @@ import courses.Courses;
 import roles.Admin;
 import roles.Professor;
 import roles.Student;
+import roles.User;
 
 public class Controller {
 	
@@ -29,12 +30,24 @@ public class Controller {
 		//create isntance of controller class 
 		Controller ct = new Controller();
 		
+		ct.fr.readFile("courseInfo.txt");
+		ct.fr.readFile("profInfo.txt");
+		ct.fr.readFile("studentInfo.txt");
+		ct.fr.readFile("adminInfo.txt");
+		
 		Scanner scanner = new Scanner(System.in);
+	
+		ct.initialLogin(scanner);
 		
+		System.out.println("Goodbye!");
 		
 
+	        
 
-	}
+		}
+		
+		
+	
 	
 	/**
 	 * a professor logs in and sees menu
@@ -45,38 +58,52 @@ public class Controller {
 	
 
 	
-	private void initialLogin(Scanner scan) {
+	private void initialLogin(Scanner scanner) {
 		
+		boolean systemRunning = true;
 		
-		
-		if(input == 1) {
-			this.loginStudent(fr, fr.getStudentInfo());
-		} 
-		
-		else if(input == 2) {
+		while(systemRunning) {
 			
-		}
-		
-		else if(input == 3) {
+			System.out.println("-------------------------\n" + "Student Management System\n" + "-------------------------\n" + "1 -- Login as student\n" 
+					+ "2 -- Login as professor\n" + "3 -- Login as admin\n" + "4 -- Quit the system\n");
 			
-			//create hasmap of usernames and passwords for student 
-			//for prof
-			//foor user 
+			System.out.println("Please enter your option, eg. '1' ");
+	
+	        String option = scanner.next().trim();
+	        
+			if(option.equals("1")) {
+				System.out.println("Student");
+				//this.loginStudent(fr, fr.getStudentInfo());
+			} 
 			
-			this.loginAdmin(fr, fr.getAdminInfo());
+			else if(option.equals("2")) {
+				System.out.println("prof");
+				
+			}
 			
+			else if(option.equals("3")) {
+				
+				System.out.println("admin");
+				
+				this.loginAdmin(scanner);
+				
+			}
+			else if(option.equals("4")) {
+				System.out.println("quit");
+				systemRunning = false;
+			}
 		}
 	}
 	
 	
-	private void loginStudent(FileInfoReader fr2, ArrayList<Student> studentInfo) {
+	/*private void loginStudent(FileInfoReader fr2, ArrayList<Student> studentInfo) {
 		// can i pass in generic arrayList<Object> and thens et to either stufent prof or admin 
 		
 		//promt for username
 		
 		
 		
-		for(Student student : studentInfo)) {
+		for(Student student : studentInfo) {
 			if(student.getUsername().equals(usernameInput)) {
 				
 				//promt for pass
@@ -87,23 +114,106 @@ public class Controller {
 			}
 		}
 		
-	}
+	}*/
 	
-	private void loginAdmin(FileInfoReader fr2, ArrayList<Admin> adminInfo) {
+	private void loginAdmin(Scanner scanner) {
 		// can i pass in generic arrayList<Object> and thens et to either stufent prof or admin 
 		
 		//promt for username
 		
+		boolean usernameFound = false;
+		boolean passwordFound = false;
 		
-		
-		for(Admin admin : adminInfo) {
-			if(student.getUsername().equals(usernameInput)) {
-				
-				//promt for pass
-				if(admin.getPassword().equals(passwordInput)) {
+		while(!(usernameFound)) {
+			
+			System.out.println("Please enter your username: ");
+			
+			String usernameInput = scanner.next().trim();
+			
+			for(Admin admin : fr.getAdminInfo()) {
+
+				if(admin.getUsername().equals(usernameInput)) {
 					
-					//call method for student experience 
+					while(!(passwordFound)) {
+						
+						System.out.println("Please enter your password: ");
+						
+						String passwordInput = scanner.next();
+						
+						if(admin.getPassword().equals(passwordInput)) {
+							
+							//call method for admin experience 
+							this.adminView(admin, scanner);
+							
+							passwordFound = true;
+						}
+					}
+					
+					usernameFound = true;
 				}
+			}	
+		}
+		
+		//this doesnt need to be here, just testing 
+		System.out.println("once quit, return to main menu");
+	
+	}
+	
+	
+	private void adminView(Admin admin, Scanner scanner) {
+			
+		boolean adminViewRunning = true;
+		
+		while(adminViewRunning) {
+			
+			System.out.println("-------------------------\n" + "Welcome, " + admin.getName() + "\n" + "-------------------------\n" + "1 -- View all courses\n" 
+					+ "2 -- Add new courses\n" + "3 -- Delete courses\n" + "4 -- Add new professor\n" + "5 -- Delete professor\n" + "6 -- Add new student\n" 
+					+ "7 -- Delete student\n" + "8 -- Return to previous menu\n");
+			
+			System.out.println("Please enter your option, eg. '1' ");
+			
+			String option = scanner.next();
+			
+
+	        
+			if(option.equals("1")) {
+				System.out.println("view all");
+
+			} 
+			
+			else if(option.equals("2")) {
+				System.out.println("add course");
+				
+			}
+			
+			else if(option.equals("3")) {
+				
+				System.out.println("delete course");
+				
+			}
+			else if(option.equals("4")) {
+				
+				System.out.println("add prof");
+				
+			}
+			else if(option.equals("5")) {
+				
+				System.out.println("delete prof");
+				
+			}
+			else if(option.equals("6")) {
+				
+				System.out.println("add student");
+				
+			}
+			else if(option.equals("7")) {
+				
+				System.out.println("delete student");
+				
+			}
+			else if(option.equals("8")) {
+				System.out.println("return to previous");
+				adminViewRunning = false;
 			}
 		}
 		
